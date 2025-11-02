e02babfb049e9b19de7e686a4c545302d84f471e
bo1929 <aliosmanberk@sabanciuniv.edu>
2025-11-02 08:45:47 -0800

    Expanded introgression.

--- simulation_data/scripts/run-introgression.sh
+++ simulation_data/scripts/run-introgression.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 NUM_THREADS=32
 
-run_introgression_cases () {
-  OUTPUT_DIR=../simulated_events/single_event/introgression-separate_blocks/
+# run_introgression_cases_difficult () {
+#   OUTPUT_DIR=../simulated_events/single_event/introgression-difficult/
+#   mkdir -p ${OUTPUT_DIR}
+#   for p in 0.05 0.10 0.15 0.20 0.25 0.30; do
+#     for b in 1; do
+#       p_lbl="${p//./}"
+#       b_lbl="${b//./}"
+#       TMPDIR=$(mktemp -d -u -p ${OUTPUT_DIR} p${p_lbl}-b${b_lbl}-XXXXX)
+#       echo ${TMPDIR}
+#       exec < ../simulated_events/introgression/selected-difficult/introgression_selected_$1.tsv || exit 1a
+#       while IFS='	' read -r recipient donor v1 v2; do
+#         python simulate_introgression.py \
+#           -s ../species_trees/estimated/SR201_default_condition/$1_1X_S201_0_haploid.caster-pair \
+#           -g ../gene_trees/SR201_default_condition/$1_1X_S201_0_haploid.gtrees \
+#           -p ${p} -b ${b} -d ${donor} -r ${recipient} \
+#           -o ${TMPDIR}b${b_lbl}-d${donor}_r${recipient}-$1_1X_S201
+#       done
+#     done
+#   done
+# }
+
+# export -f run_introgression_cases_difficult
+
+# seq 1 50 | xargs -I{} -P ${NUM_THREADS} -t bash -c "run_introgression_cases_difficult {}"
+
+run_introgression_cases_controlled () {
+  OUTPUT_DIR=../simulated_events/single_event/introgression-controlled/
   mkdir -p ${OUTPUT_DIR}
-  for p in 0.05 0.10 0.15 0.20 0.25 0.30; do
+  for p in 0.05 0.10 0.20 0.30; do
     for b in 1; do
       p_lbl="${p//./}"
       b_lbl="${b//./}"
       TMPDIR=$(mktemp -d -u -p ${OUTPUT_DIR} p${p_lbl}-b${b_lbl}-XXXXX)
       echo ${TMPDIR}
-      exec < ../simulated_events/introgression/selected/introgression_selected_$1.tsv || exit 1a
+      exec < ../simulated_events/introgression/selected-difficult/introgression_selected_$1.tsv || exit 1a
       while IFS='	' read -r recipient donor v1 v2; do
         python simulate_introgression.py \
           -s ../species_trees/estimated/SR201_default_condition/$1_1X_S201_0_haploid.caster-pair \
@@ -22,6 +47,6 @@ run_introgression_cases () {
   done
 }
 
-export -f run_introgression_cases
+export -f run_introgression_cases_controlled
 
-seq 1 50 | xargs -I{} -P ${NUM_THREADS} -t bash -c "run_introgression_cases {}"
+seq 1 50 | xargs -I{} -P ${NUM_THREADS} -t bash -c "run_introgression_cases_controlled {}"
--- simulation_data/simulated_events/introgression/select.sh
+++ simulation_data/simulated_events/introgression/select.sh
@@ -1,2 +1,6 @@
-seq 1 50 | xargs -t -I{} bash -c "head -n25 candidates/introgression_candidates_{}.tsv | shuf | head -5 > selected/introgression_selected_{}.tsv"
-seq 1 50 | xargs -t -I{} bash -c "tail -n+25 candidates/introgression_candidates_{}.tsv | head -n25 | shuf | head -5 >> selected/introgression_selected_{}.tsv"
+seq 1 50 | xargs -t -I{} bash -c "sort -gk4 candidates/introgression_candidates_{}.tsv | tail -10 > selected/introgression_selected_{}.tsv"
+seq 1 50 | xargs -t -I{} bash -c "sort -gk3 candidates/introgression_candidates_{}.tsv | head -10 >> selected/introgression_selected_{}.tsv"
+seq 1 50 | xargs -t -I{} bash -c "sort selected/introgression_selected_{}.tsv | uniq > selected/introgression_selected_{}.tsv.tmp && mv selected/introgression_selected_{}.tsv.tmp selected/introgression_selected_{}.tsv"
+
+seq 1 50 | xargs -t -I{} bash -c "head -n25 candidates/introgression_candidates_{}.tsv | shuf | head -5 > selected-difficult/introgression_selected_{}.tsv"
+seq 1 50 | xargs -t -I{} bash -c "tail -n+25 candidates/introgression_candidates_{}.tsv | head -n25 | shuf | head -5 >> selected-difficult/introgression_selected_{}.tsv"
