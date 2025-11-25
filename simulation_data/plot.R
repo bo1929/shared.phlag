b0ae0bff78505a7169727db0e9bc6e7c05620f69
bo1929 <asapci@ucsd.edu>
2025-11-25 07:31:21 -0800

    Cleaned up.

---
+++ simulation_data/plot.R
@@ -0,0 +1,186 @@
+require(ggplot2)
+require(cowplot)
+require(dplyr)
+require(reshape2)
+require(tidyr)
+require(vroom)
+
+df_intdiff <- vroom("results/introgression-difficult/all_metrics-difficult_introgression.tsv", col_names = c("method", "TN", "FP", "FN", "TP", "p", "b", "D", "R", "ST"))
+head(df_intdiff)
+di <- vroom("simulated_events/introgression/selected-difficult/all_selected.tsv", col_names = c("ST", "R", "D", "ul", "di" ))
+df <- merge(df_intdiff, di, by = c("ST", "D", "R"))
+df$P <- df$FP + df$TP
+# th <- 1000
+# df <- df %>% mutate(FN = ifelse(P > th, TP+FN, FN), TN = ifelse(P > th, TN+FP, TN), TP=ifelse(P >th, 0, TP), FP=ifelse(P > th, 0, FP))
+
+df <- df %>% mutate(
+  recall = (TP) / (TP + FN),
+  precision = (TP) / (TP + FP),
+  f1 = TP/ (TP+0.5*(FP+FN)),
+  acc=(TP+TN)/(TP+TN+FN+FP)
+)
+df %>% group_by(method) %>% summarize(wp=sum(!is.na(f1))/n(), f1=mean(f1, na.rm = TRUE), recall=mean(recall, na.rm = TRUE))
+df_s <- df %>% filter(!grepl("phylter", method))
+# df_s$method[df_s$method == "d-phlag_0010-c_min2max4-r"] <- "phlag-lambda0010-r"
+
+df_s %>% ggplot() +
+  # facet_wrap(~p) +
+  facet_wrap(~(1-round(r, 1))) +
+  stat_ecdf(aes(x = f1, color = method)) +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1") +
+  labs( y = "ECDF", color = "")
+
+df_s %>%
+  ggplot() +
+  aes(x=p, y=f1, color=method) +
+  # facet_wrap(~(1-round(r, 1))) +
+  geom_point(alpha=0.15) +
+  stat_summary() +
+  stat_summary(geom="line") +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1")
+
+df_s %>% ggplot() +
+  aes(x=di, y=f1, color=method) +
+  geom_point(alpha=0.1) +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1") +
+  # stat_smooth() +
+  stat_summary_bin(breaks = c(1, 10000, 50000, 100000, 1000000)) +
+  stat_summary_bin(breaks = c(1, 10000,  50000, 100000, 1000000), geom="line") +
+  scale_x_continuous(transform = "log", breaks = c(10, 1000, 10000, 100000)) +
+  coord_cartesian(xlim = c(1, 1000000)) +
+  labs(x="# of generations until speciation")
+
+df_s %>% ggplot() +
+  aes(x=ul, y=f1, color=method) +
+  geom_point(alpha=0.1) +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1") +
+  # stat_smooth() +
+  stat_summary_bin(breaks = 100000*c(1, 10000, 50000, 100000, 1000000)) +
+  stat_summary_bin(breaks = 100000*c(1, 10000,  50000, 100000, 1000000), geom="line") +
+  scale_x_continuous(transform = "log", breaks = 10000*c(10, 1000, 10000, 100000)) +
+  # coord_cartesian(xlim = c(1, 1000000)) +
+  labs(x="Deviation from ultrametricity")
+
+
+df_s %>% 
+  mutate(di_bin=cut(di, c(0, 10000, 25000, 50000, 75000, 100000, 150000, 250000, 600000))) %>%
+  group_by(method, di_bin) %>%
+  summarise(TP=sum(TP), FP=sum(FP), TN=sum(FN), FN=sum(FN),  recall = (TP) / (TP + FN), precision = (TP) / (TP + FP), f1 = 2 * precision * recall / (precision + recall)) %>%
+  ggplot() +
+  aes(x=di_bin, y=f1, color=method) +
+  geom_point() +
+  geom_line(aes(group = method)) +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1") +
+  labs(x="# of generations until speciation") +
+  theme(axis.text.x = element_text(angle = 30))
+
+
+df_s %>% 
+  group_by(method) %>%
+  summarise(wp = sum( (P>0) /n())) %>%
+  ggplot() +
+  aes(x=method, y=wp, fill=method) +
+  geom_col() +
+  theme_cowplot() +
+  scale_fill_brewer(palette = "Set1") +
+  labs(y="Any prediction")
+
+df <- vroom("results/population_increase_10X/all_metrics-population_increase_10X.tsv", col_names = c("method", "TN", "FP", "FN", "TP", "p", "r"))
+df$P <- df$FP + df$TP
+th <- 1000
+df <- df %>% mutate(FN = ifelse(P > th, TP+FN, FN), TN = ifelse(P > th, TN+FP, TN), TP=ifelse(P >th, 0, TP), FP=ifelse(P > th, 0, FP))
+
+df <- df %>% mutate(
+  recall = (TP) / (TP + FN),
+  precision = (TP) / (TP + FP),
+  f1 = TP/ (TP+0.5*(FP+FN)),
+  acc=(TP+TN)/(TP+TN+FN+FP)
+)
+
+df %>% group_by(method) %>% summarize(wp=sum(!is.na(f1))/n(), f1=mean(f1, na.rm = TRUE), recall=mean(recall, na.rm = TRUE))
+df_s <- df %>% filter(method %in% c("phlag_0010-c_min25max81", "phylter-k155-island_0", "BP-HMM"))
+df_s$method[df_s$method == "phlag_0010-c_min9max81"] <- "phlag_0010"
+
+df_s %>%
+  ggplot() +
+  aes(x=p*r, y=f1, color=method) +
+  facet_wrap(~(1-round(r, 1))) +
+  geom_point(alpha=0.15) +
+  stat_summary() +
+  stat_summary(geom="line") +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1")
+ 
+df_s %>% 
+  group_by(method) %>%
+  summarise(wp = sum( (P>0) /n())) %>%
+  ggplot() +
+  aes(x=method, y=wp, fill=method) +
+  geom_col() +
+  theme_cowplot() +
+  scale_fill_brewer(palette = "Set1") +
+  labs(y="Any prediction")
+
+df <- vroom("results/recombination_suppression/all_metrics-recombination_suppression.tsv", col_names = c("method", "TN", "FP", "FN", "TP", "p", "r"))
+df$P <- df$FP + df$TP
+# th <- 1000
+# df <- df %>% mutate(FN = ifelse(P > th, TP+FN, FN), TN = ifelse(P > th, TN+FP, TN), TP=ifelse(P >th, 0, TP), FP=ifelse(P > th, 0, FP))
+
+df <- df %>% mutate(
+  recall = (TP) / (TP + FN),
+  precision = (TP) / (TP + FP),
+  f1 = TP/ (TP+0.5*(FP+FN))
+)
+
+df %>% group_by(method) %>% summarize(wp=sum(!is.na(f1))/n(), f1=mean(f1, na.rm = TRUE))
+df_s <- df %>% filter(method %in% c("free_init-lambda_0005-c_min2max81", "BP-HMM")) # filter(!grepl("stpenalty", method)) %>% filter(!grepl("phylter", method))  %>% filter(!grepl("phlag", method))
+df_s$method[df_s$method == "free_init-lambda_0005-c_min2max81"] <- "Correct init"
+df_s$method[df_s$method == "argmaxx"] <- "The best topology (K=3)"
+df_s$method[df_s$method == "phlag-lambda_0010-c_min25max81"] <- "Old version"
+
+df_s %>% ggplot() +
+  # facet_wrap(~p) +
+  # facet_wrap(p~(round(r, 2))) +
+  facet_grid(p~(round(r, 2))) +
+  stat_ecdf(aes(x = precision, color = method)) +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1") +
+  labs( y = "ECDF", color = "") +
+  coord_cartesian(ylim = c(0, 0.33))
+
+df_s %>%
+  ggplot() +
+  aes(x=p, y=f1, color=method) +
+  facet_wrap(~(round(r, 2))) +
+  geom_point(alpha=0.15) +
+  stat_summary() +
+  stat_summary(geom="line") +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1")
+
+df_s %>% 
+  group_by(method) %>%
+  summarise(wp = sum( (P>0) /n())) %>%
+  ggplot() +
+  aes(x=method, y=wp, fill=method) +
+  geom_col() +
+  theme_cowplot() +
+  scale_fill_brewer(palette = "Set1") +
+  labs(y="Any prediction")
+
+df_s %>% 
+  group_by(method, p, r) %>%
+  summarise(TP=sum(TP), FP=sum(FP), TN=sum(FN), FN=sum(FN),  recall = (TP) / (TP + FN), precision = (TP) / (TP + FP), f1 = 2 * precision * recall / (precision + recall)) %>%
+  ggplot() +
+  facet_wrap(~(round(r, 2))) +
+  aes(x=p, y=f1, color=method) +
+  geom_point() +
+  geom_line(aes(group = method)) +
+  theme_minimal_grid() +
+  scale_color_brewer(palette = "Set1") +
+  theme(axis.text.x = element_text(angle = 30))
